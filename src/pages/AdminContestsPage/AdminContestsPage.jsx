e892fab96f4e665516707d5088233eb3e4222e56
Lorelei <lorelei@basken.com>
2023-10-15 19:10:22 -0400

    added functionality to create a new contest

--- src/pages/AdminContestsPage/AdminContestsPage.jsx
+++ src/pages/AdminContestsPage/AdminContestsPage.jsx
@@ -4,10 +4,12 @@ import AddIcon from '@mui/icons-material/Add';
 import {useEffect, useState} from "react";
 import "./AdminContestsPage.css";
 import {Link} from "react-router-dom";
+import ServiceClient from "../../ServiceClient.js";
 
 const columns = [
     {field: 'name'},
-    {field: 'start_date'},
+  {field: 'description'},
+  {field: 'start_date'},
     {field: 'end_date'}
 ];
 
@@ -15,12 +17,9 @@ export default function AdminContestsPage() {
     const [rows, setRows] = useState([]);
 
     useEffect(() => {
-        fetch("https://api-zgnozpscca-uc.a.run.app/contests")
-            .then(response => response.json())
-            .then(data => {
-                setRows(data);
-            })
-            .catch(error => console.log(error.message))
+      ServiceClient.request("/contest")
+        .then(data => setRows(data))
+        .catch(error => console.log(error.message));
     }, [])
 
     return <div>
--- src/pages/AdminCreateContestPage/AdminCreateContestPage.jsx
+++ src/pages/AdminCreateContestPage/AdminCreateContestPage.jsx
@@ -3,31 +3,34 @@ import {Field, Form, Formik} from "formik";
 import {Button, TextField} from "@mui/material";
 import {Link} from "react-router-dom";
 import {MuiChipsInput} from "mui-chips-input";
-
-
-
-const onSubmit = (formState, formEvent) => {
-    console.log("Form Submitted");
-    console.log(formState, formEvent);
-};
+import ServiceClient from "../../ServiceClient.js";
+import DatePicker from "react-multi-date-picker";
 
 export default function AdminCreateContestPage() {
-    const [chips, setChips] = useState([]);
+    // const [chips, setChips] = useState([]);
+    //
+    // function handleChange(newChips) {
+    //   setChips(newChips);
+    // }
 
-    const handleChange = (newChips) => {
-        setChips(newChips);
-    }
+  function onSubmit(formState, formEvent) {
+    ServiceClient.request("/contest", "POST", formState)
+      .then(data => console.log(data))
+      .catch(error => console.log(error.message));
+  }
 
     return <div>
         <h1>Create New Contest</h1>
-        <Formik initialValues={{title: "", description: "", cookie: "", start_date: 0, end_date: 0}} onSubmit={onSubmit}>
+        <Formik initialValues={{name: "", description: "", cookie: "", start_date: 0, end_date: 0}} onSubmit={onSubmit}>
             <Form>
-                <Field name="title">{(props) => <TextField required fullWidth type="text" label="Title" {...props.field} />}</Field>
-                <Field name="description">{(props) => <TextField required fullWidth type="text" label="Description" {...props.field} />}</Field>
-                <Field name="start_date">{(props) => <TextField required fullWidth type="date" label="Contest Start Date" {...props.field} />}</Field>
-                <Field name="end_date">{(props) => <TextField required fullWidth type="date" label="Contest End Date" {...props.field} />}</Field>
-                <Field name="cookie">{(props) => <MuiChipsInput required size="small" label="Cookies" {...props.field} value={chips} onChange={handleChange} />}</Field>
-                <Button variant="contained" type="submit">Create Contest</Button>
+              <Field name="name">{(props) => <TextField required fullWidth type="text" label="Title" {...props.field} />}</Field>
+              <Field name="description">{(props) => <TextField required fullWidth type="text" label="Description" {...props.field} />}</Field>
+              {/*<Field name="start_date">{(props) => <TextField required fullWidth type="date" label="Contest Start Date" {...props.field} />}</Field>*/}
+              {/*<Field name="end_date">{(props) => <TextField required fullWidth type="date" label="Contest End Date" {...props.field} />}</Field>*/}
+              {/*<Field name="cookie">{(props) => <MuiChipsInput required size="small" label="Cookies" {...props.field} value={chips} onChange={handleChange} />}</Field>*/}
+              <Field name="start_date">{(props) => <DatePicker range {...props.field} />}</Field>
+              <Field name="end_date">{(props) => <DatePicker range rangeHover {...props.field} />}</Field>
+              <Button variant="contained" type="submit">Create Contest</Button>
             </Form>
         </Formik>
         <button><Link to="/admin-contests">Cancel</Link></button>
