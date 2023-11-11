b9b2ca4f2a8d0ed49efeb8d29e0f66bd07bc34b7
Dana Basken <dana@pridelands.com>
2023-11-11 14:13:24 -0500

    report on existing email during invitation, validate email field

--- src/pages/InviteUserPage/InviteUserPage.jsx
+++ src/pages/InviteUserPage/InviteUserPage.jsx
@@ -14,7 +14,6 @@ export default function InviteUserPage() {
   const [invitation, setInvitation] = useState();
 
   async function onSubmit(values) {
-    console.log("onSubmit", values);
     if (Utilities.isEmpty(values.email)) { return; }
     try {
       const response = await ServiceClient.request("/user/invite", "POST", values);
@@ -29,7 +28,7 @@ export default function InviteUserPage() {
 
   function showError(error) {
     let message = error.message;
-    if (error.code === 409) { message = "There is at least one outstanding invitation for that email."; }
+    if (error.code === 409) { message = "An account already exists for that email."; }
     const dialog = showModal(
       ErrorDialog,
       {
@@ -41,27 +40,42 @@ export default function InviteUserPage() {
     );
   }
 
+  function validate(values) {
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  }
+
+  function renderInvitationLink() {
+    if (!invitation) { return; }
+    return <div className="invite-user-invitation-link">
+      <pre>
+      {invitation}
+      </pre>
+    </div>;
+  }
+
   const values = {
     email: ""
   };
 
   return <div className="invite-user-page">
     <div className="invite-user-page-title">Invite User</div>
-    <Formik enableReinitialize initialValues={values} onSubmit={onSubmit}>
+    <Formik enableReinitialize initialValues={values} validateOnMount validate={validate} onSubmit={onSubmit}>
       {props => {
         return <>
           <Form className="invite-user-form">
             <Field name="email">{(props) => <TextField className="invite-user-form-email" required fullWidth variant="standard" type="email" label="Email" {...props.field} />}</Field>
-            <Button variant="contained" onClick={() => props.submitForm()}>Create Invitation Link</Button>
+            <Button variant="contained" disabled={!props.isValid} onClick={() => props.submitForm()}>Create Invitation Link</Button>
           </Form>
         </>
       }}
     </Formik>
-    <div className="invite-user-invitation-link">
-      <pre>
-      {invitation}
-      </pre>
-    </div>
+    {renderInvitationLink()}
   </div>
 
 }
