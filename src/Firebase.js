79bb4f06f3a7c9daa211a541df43da8b079c9248
Lorelei Basken <lorelei@basken.com>
2024-03-19 15:48:46 -0400

    created config.json, updated gitignore

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,16 +2,17 @@ import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 import * as firebaseui from "firebaseui";
 import {EventBus, State} from "@d4lton/node-frontend";
+import {apiKey, appId} from "../config.json";
 
 export default class Firebase {
 
   static firebaseConfig = {
-    apiKey: "AIzaSyCd6phPgYrE3PRJakSHnLgYuWZG2ooTIwk",
+    apiKey: apiKey,
     authDomain: "confection-selection.firebaseapp.com",
     projectId: "confection-selection",
     storageBucket: "confection-selection.appspot.com",
     messagingSenderId: "773960931020",
-    appId: "1:773960931020:web:5bfb26e810244012d05469",
+    appId: appId,
     measurementId: "G-F8VBR4NERR"
   };
 
