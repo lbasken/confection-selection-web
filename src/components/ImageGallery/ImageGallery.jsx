74d6b97e725b69c9d226abd40ad569ed287d5f2c
Lorelei Basken <lorelei@basken.com>
2024-12-31 14:22:08 -0500

    added christmas 2024 winners to Hall of Fame

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,13 +2,24 @@ import * as React from 'react';
 import "./ImageGallery.css";
 
 export default function ImageGallery() {
-  return <div className="polaroid">
-    <img src="https://webdev-images.s3.us-east-1.amazonaws.com/confection-selection/chr2023.JPG" alt="christmas 2023" />
-    <div className="container">
-      <h2>Christmas 2023 winners!</h2>
-      <p><b><i>Best Tasting</i></b>... Celeste's Powder Puff Cookies</p>
-      <p><b><i>Most Creative</i></b>... Chris' 'Letters to Santa' Cookies</p>
-      <p><b><i>Most Festive</i></b>... Halley's Grinch Cookies</p>
+  return <div>
+    <div className="polaroid">
+      <img src="https://webdev-images.s3.us-east-1.amazonaws.com/confection-selection/chr2023.JPG" alt="christmas 2023"/>
+      <div className="container">
+        <h2>Christmas 2023 winners!</h2>
+        <p><b><i>Best Tasting</i></b>... Celeste's Powder Puff Cookies</p>
+        <p><b><i>Most Creative</i></b>... Chris' 'Letters to Santa' Cookies</p>
+        <p><b><i>Most Festive</i></b>... Halley's Grinch Cookies</p>
+      </div>
+    </div>
+    <div className="polaroid">
+      <img src="https://webdev-images.s3.us-east-1.amazonaws.com/confection-selection/chr2024.jpg" alt="christmas 2023"/>
+      <div className="container">
+        <h2>Christmas 2024 winners!</h2>
+        <p><b><i>Best Tasting</i></b>... Riley's Hot Cocoa Cookies</p>
+        <p><b><i>Most Creative</i></b>... Halley's Santa Cookies</p>
+        <p><b><i>Most Festive</i></b>... Riley's Hot Cocoa Cookies</p>
+      </div>
     </div>
   </div>
 }
